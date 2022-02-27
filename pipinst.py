4b1003b976f104ff5d4d97b4f9cf096ad785cf37
Fisherman386 <carlospuenteg@gmail.com>
2022-02-27 17:43:30 +0100

    Update pipinst.py

--- pipinst.py
+++ pipinst.py
@@ -33,6 +33,13 @@ def pipin(*mods):
                             raise Exception
                     except:
                         unable.append(mod)
+                        
+
+"""
+You can this to your code so that it installs the modules if it can or stops the execution if it can't
+if pipin("pygame","neat-python"):
+    exit()
+"""
 
     if (not unable):
         return 0  # print("\nInstallation COMPLETE!\n")
